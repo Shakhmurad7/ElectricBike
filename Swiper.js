4c5eef3d9f4fd3d7443800a3e4a34cd190861315
Shakhmurad <muradb356@gmail.com>
2024-08-29 00:25:03 +0400

    add swiper

---
+++ Swiper.js
@@ -0,0 +1,24 @@
+const swiperEl = document.querySelector('swiper-container')
+Object.assign(swiperEl, {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 2000,
+  },
+  loop: true,
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  },
+});
+swiperEl.initialize();
