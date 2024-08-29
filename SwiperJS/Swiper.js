1009b4ad7da7a18ceec01d46d1d9933b22ed5739
Shakhmurad <muradb356@gmail.com>
2024-08-29 15:31:17 +0400

    add swiper to cart

--- SwiperJS/Swiper.js
+++ SwiperJS/Swiper.js
@@ -22,3 +22,28 @@ Object.assign(swiperEl, {
   },
 });
 swiperEl.initialize();
+
+const swiperEls = document.querySelector('.mySwiper.bike')
+Object.assign(swiperEls, {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  },
+});
+swiperEls.initialize();
--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-const block = document.querySelector('.container-box')
+const block = document.querySelector('.mySwiper.bike')
 const url = 'https://electric-bike-db.vercel.app/posts-bicycle'
 
 axios.get(url).then(({data})=>{
     data.forEach(elem => {
         block.innerHTML += `
-            <div class="block">
-                <div class="block-top-tetx">
-                    <img src="./img/${elem.logo}.svg">
+        <swiper-slide class="swiper-slides" >
+         <div class="block">
+            <div class="block-top-tetx">
+            <img src="./img/${elem.logo}.svg">
                     <p>${elem.item}</p>
                 </div>
                 <div class="img-block">
                     <img src="./img/${elem.img}.png" alt="">
-                </div>
-                <p class="text-item">${elem.text}</p>
-                <p class="price">${elem.price}$</p>
-            </div>
-        `
+                    </div>
+                    <p class="text-item">${elem.text}</p>
+                    <p class="price">${elem.price}$</p>
+                    </div>
+        </swiper-slide>
+                    `
     });
 })
