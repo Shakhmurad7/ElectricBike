015a610f3425ce5b7dba5373b54545616e4bfabd
Shakhmurad <muradb356@gmail.com>
2024-09-10 23:55:49 +0400

    add section-bike

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const block = document.querySelector('.mySwiper.bike');
+const sectionBikeBlock = document.querySelector('.section-bike-block')
 const url = 'https://electric-bike-db.vercel.app/posts-bicycle';
 
 axios.get(url).then(({ data }) => {
@@ -20,6 +21,27 @@ axios.get(url).then(({ data }) => {
                 <button>1 klik ile</button>
             </div>
         </swiper-slide>
+        `;
+    });
+});
+
+axios.get(url).then(({ data }) => {
+    data.slice(0,3).forEach(elem => {
+        sectionBikeBlock.innerHTML += `
+            <div class="block">
+                <div class="block-top-tetx">
+                    <img src="./img/${elem.logo}.svg">
+                    <p class="text-p" style="color: ${elem.item === 'В наличии' ? 'green' : 'red'};">
+                        ${elem.item}
+                    </p>
+                </div>
+                <div class="img-block">
+                    <img src="./img/${elem.img}.png" alt="">
+                </div>
+                <p class="text-item">${elem.text}</p>
+                <p class="price">${elem.price}$</p>
+                <button>1 klik ile</button>
+            </div>
         `;
     });
 });
--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
-figma - https://www.figma.com/design/3TdnaZPCgaTzqzPZ3p2iCU/%D0%92%D0%B5%D0%BB%D0%BE%D1%81%D0%B8%D0%BF%D0%B5%D0%B4%D1%8B-~23000-27000-(Copy)?node-id=1874-266&t=D9GPdysvvqTo1KJi-0# ElectricBike
+figma - 
+https://www.figma.com/design/3TdnaZPCgaTzqzPZ3p2iCU/%D0%92%D0%B5%D0%BB%D0%BE%D1%81%D0%B8%D0%BF%D0%B5%D0%B4%D1%8B-~23000-27000-(Copy)?node-id=1874-266&t=D9GPdysvvqTo1KJi-0# ElectricBike
