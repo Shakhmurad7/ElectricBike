1ba3dbaec1d0e5b3fdced9bfaea129a5dae48de5
Shakhmurad <muradb356@gmail.com>
2024-11-12 02:07:08 +0400

    add swiper helmet

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const block = document.querySelector('.mySwiper.bike');
 const sectionBikeBlock = document.querySelector('.section-bike-block')
+const helmetBox = document.querySelector('.mySwiper.helmetBox')
 const url = 'https://electric-bike-db.vercel.app/posts-bicycle';
 
 axios.get(url).then(({ data }) => {
@@ -18,7 +19,7 @@ axios.get(url).then(({ data }) => {
                 </div>
                 <p class="text-item">${elem.text}</p>
                 <p class="price">${elem.price}$</p>
-                <button>1 klik ile</button>
+                 <button style='display: ${elem.item === 'В наличии' ? 'flex': 'none'}'>1 klik ile</button>
             </div>
         </swiper-slide>
         `;
@@ -40,8 +41,31 @@ axios.get(url).then(({ data }) => {
                 </div>
                 <p class="text-item">${elem.text}</p>
                 <p class="price">${elem.price}$</p>
-                <button>1 klik ile</button>
+                 <button style='display: ${elem.item === 'В наличии' ? 'flex': 'none'}'>1 klik ile</button>
             </div>
         `;
     });
 });
+
+
+axios.get('https://electric-bike-db.vercel.app/postes-helmet').then(({data})=>{
+    data.forEach(elem =>{
+        helmetBox.innerHTML  += `
+        <swiper-slide class="swiper-slides">
+            <div class="block">
+                <div class="block-top-tetx">
+                    <p class="text-p" style="color: ${elem.item === 'В наличии' ? 'green' : 'red'};">
+                        ${elem.item}
+                    </p>
+                </div>
+                <div class="img-block">
+                    <img src="./img/${elem.img}.png" alt="">
+                </div>
+                <p class="text-item">${elem.text}</p>
+                <p class="price">${elem.price}$</p>
+                <button style='display: ${elem.item === 'В наличии' ? 'flex': 'none'}'>1 klik ile</button>
+            </div>
+        </swiper-slide>
+        `
+    })
+})
